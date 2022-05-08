c117814db7681f1f23c243f1f7274176ab182c93
Per Jahnstedt <perjah20@student.hh.se>
2022-05-08 11:18:32 +0200

    Added comments to the constructor

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -4,8 +4,15 @@ public abstract class Game {
     private GameModel gameModel;
     private GameGUI gameGUI;
 
+    /**
+     * Constructs a new Game object with supplied GameGUI and GameModel.
+     * @param aGameModel - A GameModel to represent the game in an abstract way.
+     * @param aGameGUI - A interactive GUI that allows you to see and play the game.
+     */
     public Game(GameModel aGameModel, GameGUI aGameGUI){
         gameModel = aGameModel;
         gameGUI = aGameGUI;
     }
+
+
 }
