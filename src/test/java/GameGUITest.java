dc2d3756e4bd7b8279db56bdb6608b19f937f464
Per Jahnstedt <perjah20@student.hh.se>
2022-05-09 00:39:16 +0200

    Added the testFolder I have been using. It is possible to run this to see the GameGUI in action with some sound effects when pressing the buttons.

---
+++ src/test/java/GameGUITest.java
@@ -0,0 +1,57 @@
+import org.junit.jupiter.api.Test;
+
+import javax.sound.sampled.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class GameGUITest {
+
+    @Test
+    public void testGUI () {
+
+    }
+
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        File audioFile = new File("src/test/java/footstep_carpet_002 (1).wav").getAbsoluteFile();
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(audioFile);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioInputStream);
+        new TileGameGUI(4,4) {
+
+            @Override
+            public void updateThisGameObserver(int[][] gameState) {
+
+            }
+
+            @Override
+            public void northButtonPressed() {
+                System.out.println("NorthButtonPressed");
+                clip.start();
+                clip.setMicrosecondPosition(0);
+            }
+
+            @Override
+            public void eastButtonPressed() {
+                System.out.println("EastButtonPressed");
+                clip.start();
+                clip.setMicrosecondPosition(0);
+            }
+
+            @Override
+            public void southButtonPressed() {
+                System.out.println("SouthButtonPressed");
+                clip.start();
+                clip.setMicrosecondPosition(0);
+            }
+
+            @Override
+            public void westButtonPressed() {
+                System.out.println("WestButtonPressed");
+                clip.start();
+                clip.setMicrosecondPosition(0);
+            }
+        };
+    }
+
+}
