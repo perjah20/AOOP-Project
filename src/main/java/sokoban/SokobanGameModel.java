2e3dff7ce317ee30b3a703820b65b02724a90a66
Per Jahnstedt <perjah20@student.hh.se>
2022-05-12 01:15:07 +0200

    SokobanGameModel now uses enums in directions and SokobanInfo for values.

--- src/main/java/sokoban/SokobanGameModel.java
+++ src/main/java/sokoban/SokobanGameModel.java
@@ -8,7 +8,7 @@ import java.util.Stack;
 import static sokoban.SokobanInfo.BOX;
 import static sokoban.SokobanInfo.COBBLESTONE;
 
-public class SokobanGameModel extends TileGameModel implements GameObserver {
+public class SokobanGameModel extends TileGameModel {
     /**
      * Constructs a GameModel object.
      *
@@ -17,7 +17,8 @@ public class SokobanGameModel extends TileGameModel implements GameObserver {
      */
     public SokobanGameModel(int rows, int columns) {
         super(rows, columns);
-        oldTile = SokobanInfo.SAND;
+        tileStack = new Stack<>();
+        tileStack.push(SokobanInfo.SAND);
     }
 
     private void setCharacterPosition() {
@@ -35,51 +36,124 @@ public class SokobanGameModel extends TileGameModel implements GameObserver {
     }
 
     public void moveCharacter(directions direction){
+        // lokalisera karaktären
+        // Kolla vilken riktning vi ska gå
+        // Kolla vilken tile som finns på nästa position
+
+        setCharacterPosition();
         switch (direction){
-            case NORTH-> move(UP,0);
-            case WEST -> move(0,LEFT);
-            case EAST -> move(0, RIGHT);
-            case SOUTH-> move(DOWN,0);
-        }
-    }
+            case NORTH:
+                if (isValidMove(-1,0));
+                nextLocation = getTileState(playerLocationY -1, playerLocationX);
+                currentLocation = getTileState(playerLocationY,playerLocationX);
+                //int previousLocation = currentLocation;
+                currentLocation = tileStack.pop();
+                if (nextLocation == 10) {
+                    tileStack.push(SAND);
+                    moveCrate(direction);
+                }
+                if (nextLocation == 15) {
+                    tileStack.push(DOT);
+                    moveCrate(direction);
+                }
+                nextLocation = CHARACTER;
+                setTileState(currentLocation,playerLocationY,playerLocationX);
+                setTileState(nextLocation,playerLocationY -1, playerLocationX);
+                break;
 
-    private void move(int Y, int X) {
-        setTileState(oldTile,playerLocationY,playerLocationX); // Restore current tile.
-        int nextRow = playerLocationY + Y, nextNextRow = playerLocationY + Y * 2;
-        int nextColumn = playerLocationX + X, nextNextColumn = playerLocationX + X * 2;
-        if (getTileState(nextRow,nextColumn) < COBBLESTONE) {
-            switch (getTileState(nextRow, nextColumn)) {
-                case BOX:
-                    if (getTileState(nextNextRow,nextNextColumn) < BOX) {
-                        if (getTileState(nextNextRow, nextNextColumn) == SokobanInfo.DOT)
-                            setTileState(SokobanInfo.FILLEDBOX, nextNextRow, nextNextColumn);
-                        else
-                            setTileState(BOX, nextNextRow, nextNextColumn);
-                        setTileState(SokobanInfo.SAND, nextRow, nextColumn);
-                    }
-                    break;
-                case SokobanInfo.FILLEDBOX:
-                    if (getTileState(nextNextRow,nextNextColumn) < BOX) {
-                        if (getTileState(nextNextRow, nextNextColumn) == SokobanInfo.DOT)
-                            setTileState(SokobanInfo.FILLEDBOX, nextNextRow, nextNextColumn);
-                        else
-                            setTileState(BOX, nextNextRow, nextNextColumn);
-                        setTileState(SokobanInfo.DOT, nextRow, nextColumn);
-                    }
-                    break;
+            case WEST:
+                if (isValidMove(0,-1));
+                nextLocation = getTileState(playerLocationY, playerLocationX -1);
+                currentLocation = getTileState(playerLocationY,playerLocationX);
+                //int previousLocation = currentLocation;
+                currentLocation = tileStack.pop();
+                if (nextLocation == 10) {
+                    tileStack.push(SAND);
+                    moveCrate(direction);
+                }
+                if (nextLocation == 15) {
+                    tileStack.push(DOT);
+                    moveCrate(direction);
+                }
+                nextLocation = CHARACTER;
+                setTileState(currentLocation,playerLocationY,playerLocationX);
+                setTileState(nextLocation,playerLocationY, playerLocationX -1);
+                break;
 
-            }
-            oldTile = getTileState(nextRow, nextColumn);
-            setTileState(SokobanInfo.PLAYER, nextRow, nextColumn);
-            setCharacterPosition();
-            updateObservers();
+            case EAST:
+                if (isValidMove(0,1));
+                nextLocation = getTileState(playerLocationY, playerLocationX +1);
+                currentLocation = getTileState(playerLocationY,playerLocationX);
+                //int previousLocation = currentLocation;
+                currentLocation = tileStack.pop();
+                if (nextLocation == 10) {
+                    tileStack.push(SAND);
+                    moveCrate(direction);
+                }
+                if (nextLocation == 15) {
+                    tileStack.push(DOT);
+                    moveCrate(direction);
+                }
+                nextLocation = CHARACTER;
+                setTileState(currentLocation,playerLocationY,playerLocationX);
+                setTileState(nextLocation,playerLocationY, playerLocationX +1);
+                break;
+
+            case SOUTH:
+                if (isValidMove(1,0))
+                    nextLocation = getTileState(playerLocationY +1, playerLocationX);
+                currentLocation = getTileState(playerLocationY,playerLocationX);
+                //int previousLocation = currentLocation;
+                currentLocation = tileStack.pop();
+                if (nextLocation == 10) {
+                    tileStack.push(SAND);
+                    moveCrate(direction);
+                }
+                if (nextLocation == 15) {
+                    tileStack.push(DOT);
+                    moveCrate(direction);
+                }
+                nextLocation = CHARACTER;
+                setTileState(currentLocation,playerLocationY,playerLocationX);
+                setTileState(nextLocation,playerLocationY +1, playerLocationX);
+                break;
         }
+        updateObservers();
     }
 
     private boolean isValidMove(int Y, int X) {
-        if (getTileState(playerLocationY + Y,playerLocationX + X) <= BOX)
-            return (getTileState(playerLocationY + Y * 2,playerLocationX + X * 2) < BOX);
+        int[][] gameGrid = getGameState();
+        if (gameGrid[playerLocationY + Y][playerLocationX + X] < COBBLESTONE)
+            return (gameGrid[playerLocationY + Y*2][playerLocationX + X*2] < BOX);
         return false;
+    }
+
+    public void moveCrate(directions direction) {
+        //int[][] gameGrid = getGameState();
+        switch (direction){
+            case NORTH:
+                if (isValidMove(-1,0));
+                nextLocation = getTileState(playerLocationY -1, playerLocationX);
+                currentLocation = getTileState(playerLocationY,playerLocationX);
+                int previousLocation = currentLocation;
+                currentLocation = tileStack.pop();
+                if (nextLocation == 10) {
+                    tileStack.push(SAND);
+                    moveCrate(directions.NORTH);
+                }
+                if (nextLocation == 15) tileStack.push(DOT);
+                // repa
+                break;
+            case WEST:
+                if (isValidMove(0,-1));
+                break;
+            case EAST:
+                if (isValidMove(0,1));
+                break;
+            case SOUTH:
+                if (isValidMove(1,0))
+                    break;
+        }
     }
 
     @Override
@@ -92,16 +166,15 @@ public class SokobanGameModel extends TileGameModel implements GameObserver {
 
     }
 
-    private int oldTile;
-    private int playerLocationY;
-    private int playerLocationX;
+    private Stack<Integer> tileStack;
+    int playerLocationY;
+    int playerLocationX;
+    int currentLocation;
+    int nextLocation;
 
-    final int UP = -1, LEFT = -1, RIGHT = 1, DOWN = 1;
+    int SAND = 1, CHARACTER = 2, DOT = 5, BOX = 10, FILLEDBOX = 15, COBBLESTONE = 20;
 
-    @Override
-    public void updateGameObserver(int[][] gameState) {
-        setCharacterPosition();
-    }
-
+    //    final int NORTH = 1, WEST = 2, EAST = 3, SOUTH = 4;
     enum directions {NORTH, WEST ,EAST, SOUTH}
+
 }
