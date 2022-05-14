901c7c7bb81fbf907af5bf8100d160e4a1da01d0
RasEkl <rasekl19@student.hh.se>
2022-05-14 08:13:17 +0200

    Small changes

--- src/main/java/sokoban/SokobanGame.java
+++ src/main/java/sokoban/SokobanGame.java
@@ -13,7 +13,7 @@ public class SokobanGame  {
         sokobanGameGUI.setController(new SokobanController(sokobanGameGUI,sokobanGameModel));
         sokobanGameModel.addGameObserver(sokobanGameGUI);
         sokobanGameModel.addGameObserver(new SokobanConsole());
-        sokobanGameModel.updateGameGrid(SokobanInfo.level1);
+        sokobanGameModel.updateGameGrid(SokobanInfo.getLevel(0));
 
     }
 }
