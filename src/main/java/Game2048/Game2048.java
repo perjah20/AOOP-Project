b363392d249983dcd543c1fb3d0ed94e4dbb5316
Per Jahnstedt <perjah20@student.hh.se>
2022-05-18 00:17:29 +0200

    Added a grid to test the game with.

--- src/main/java/Game2048/Game2048.java
+++ src/main/java/Game2048/Game2048.java
@@ -5,19 +5,19 @@ import tileGame.TileGameController;
 
 public class Game2048 {
     private int[][] start = {
-            {0,0,0,2},
-            {0,0,0,2},
-            {0,0,0,2},
-            {0,0,0,2}
-        };
-
+            {1024,1024,512,256},
+            {128,64,32,16},
+            {8,4,2,0},
+            {0,0,0,0},
+    };
     public Game2048() {
         GameModel2048 gameModel2048 = new GameModel2048();
         GameGUI2048 gameGUI2048 = new GameGUI2048(new TileGameController(gameModel2048));
         gameModel2048.addGameObserver(gameGUI2048);
         gameModel2048.addGameObserver(new Console2048());
-        gameModel2048.updateGameGrid(start);
+        //gameModel2048.updateGameGrid(start);
         //gameModel2048.move(3);
+        gameModel2048.updateObservers();
     }
 
     public static void main(String[] args) {
