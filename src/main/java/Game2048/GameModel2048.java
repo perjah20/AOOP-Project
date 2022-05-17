c7e6ec0a688f652d3fdd5019a5f790e1211565ec
Per Jahnstedt <perjah20@student.hh.se>
2022-05-17 12:39:56 +0200

    Implemented a move method, needs testing to ensure its working.

--- src/main/java/Game2048/GameModel2048.java
+++ src/main/java/Game2048/GameModel2048.java
@@ -7,116 +7,136 @@ public class GameModel2048 extends TileGameModel {
 
     }
 
-    public void move(int direction) {
+    public void move(Direction direction) {
         System.out.println("invoked");
-        int tileMerged = 0;
-        int col = 0;
-        int row = 0;
-        int currentTileValue = 0;
-        int nextTileValue = 0;
         switch (direction) {
-            case 1:
-                // Move Left
-                for (row = 0; row < 4; row++) {
-                    col = 0;
-                    tileMerged = 0;
-                    while(col < 3) {
-                        currentTileValue = this.getTileState(row,col);
-                        nextTileValue = this.getTileState(row,col+1);
-                        if(currentTileValue == 0 && nextTileValue != 0)
-                        {
-                            setTileState(nextTileValue,row,col);
-                            setTileState(0,row,col + 1);
-                            //col = tileMerged;
-                            col = 0;
-                        }
-                        else if (currentTileValue == nextTileValue && currentTileValue != 0)
-                        {
-                            setTileState(currentTileValue + nextTileValue,row,col);
-                            setTileState(0,row,col + 1);
-                            //tileMerged = col;
-                        }
-                        else col++;
-                    }
-                }
-                break;
-            case 2:
-                // Move right
-                for (row = 0; row < 4; row++) {
-                    col = 3;
-                    tileMerged = 3;
-                    while(col > 0) {
-                        currentTileValue = this.getTileState(row,col);
-                        nextTileValue = this.getTileState(row,col - 1);
-                        if(currentTileValue == 0 && nextTileValue != 0)
-                        {
-                            setTileState(nextTileValue,row,col);
-                            setTileState(0,row,col - 1);
-                            //col = tileMerged;
-                            col = 3;
-                        }
-                        else if (currentTileValue == nextTileValue)
-                        {
-                            setTileState(currentTileValue + nextTileValue,row,col);
-                            setTileState(0,row,col - 1);
-                            tileMerged = col;
-                        }
-                        else col--;
-                    }
-                }
-                break;
-            case 3:
-                // Move up
-                for (col = 0; col < 4; col++) {
-                    row = 0;
-                    tileMerged = 0;
-                    while(row < 3) {
-                        currentTileValue = this.getTileState(row,col);
-                        nextTileValue = this.getTileState(row+1,col);
-                        if(currentTileValue == 0 && nextTileValue != 0)
-                        {
-                            setTileState(nextTileValue,row,col);
-                            setTileState(0,row+1,col);
-                            //row = tileMerged;
-                            row = 0;
-                        }
-                        else if (currentTileValue == nextTileValue)
-                        {
-                            setTileState(currentTileValue + nextTileValue,row,col);
-                            setTileState(0,row+1,col);
-                            tileMerged = row;
-                        }
-                        else row++;
-                    }
-                }
-                break;
-            case 4:
-                // Move down
-                for (col = 0; col < 4; col++) {
-                    row = 3;
-                    tileMerged = 3;
-                    while(row > 0) {
-                        currentTileValue = this.getTileState(row,col);
-                        nextTileValue = this.getTileState(row-1,col);
-                        if(currentTileValue == 0 && nextTileValue != 0)
-                        {
-                            setTileState(nextTileValue,row,col);
-                            setTileState(0,row-1,col);
-                            //row = tileMerged;
-                            row = 3;
-                        }
-                        else if (currentTileValue == nextTileValue)
-                        {
-                            setTileState(currentTileValue + nextTileValue,row,col);
-                            setTileState(0,row-1,col);
-                            tileMerged = row;
-                        }
-                        else row--;
-                    }
-                }
-                break;
+            case LEFT -> {
+                shiftLeft();
+                mergeLeft();
+                shiftLeft();
+            }
+            case RIGHT -> {
+                shiftRight();
+                mergeRight();
+                shiftRight();
+            }
+            case UP -> {
+                shiftUp();
+                mergeUp();
+                shiftUp();
+            }
+            case DOWN -> {
+                shiftDown();
+                mergeDown();
+                shiftDown();
+            }
         }
-    updateObservers();
+        updateObservers();
     }
 
+    private void shiftLeft() {
+        for (int row = 0; row < 4; row++) {
+            for (int col = 0; col < 3;) {
+                if (shiftTiles(row,col,0,1))
+                    col = 0;
+                else
+                    col++;
+            }
+        }
+    }
+
+    private void mergeLeft() {
+        for (int row = 0; row < 4; row++) {
+            for (int col = 0; col < 3;) {
+                mergeTiles(row,col,0,1);
+                col++;
+            }
+        }
+    }
+
+    private void shiftRight() {
+        for (int row = 0; row < 4; row++) {
+            for (int col = 3; col > 0;) {
+                if (shiftTiles(row,col,0,-1))
+                    col = 3;
+                else
+                    col--;
+            }
+        }
+    }
+
+    private void mergeRight() {
+        for (int row = 0; row < 4; row++) {
+            for (int col = 3; col > 0;) {
+                mergeTiles(row,col,0,-1);
+                col--;
+            }
+        }
+    }
+
+    private void shiftUp() {
+        for (int col = 0; col < 4; col++) {
+            for (int row = 0; row < 3;) {
+                if (shiftTiles(row,col,1,0))
+                    row = 0;
+                else
+                    row++;
+            }
+        }
+    }
+
+    private void mergeUp() {
+        for (int col = 0; col < 4; col++) {
+            for (int row = 0; row < 3;) {
+                mergeTiles(row,col,1,0);
+                row++;
+            }
+        }
+    }
+
+    private void shiftDown() {
+        for (int col = 0; col < 4; col++) {
+            for (int row = 3; row > 0;) {
+                if (shiftTiles(row,col,-1,0))
+                    row = 3;
+                else
+                    row--;
+            }
+        }
+    }
+
+    private void mergeDown() {
+        for (int col = 0; col < 4; col++) {
+            for (int row = 3; row > 0;) {
+                mergeTiles(row,col,-1,0);
+                row--;
+            }
+        }
+    }
+
+    private void mergeTiles(int row, int col, int offsetY, int offsetX) {
+        int currentTileValue = this.getTileState(row, col);
+        int nextTileValue = this.getTileState(row + offsetY, col + offsetX);
+        if (currentTileValue == nextTileValue && currentTileValue != 0) {
+            setTileState(currentTileValue + nextTileValue, row, col);
+            setTileState(0, row + offsetY, col + offsetX);
+        }
+    }
+
+    private boolean shiftTiles(int row, int col, int offsetY, int offsetX) {
+
+
+        int currentTileValue = this.getTileState(row, col);
+        int nextTileValue = this.getTileState(row + offsetY, col + offsetX);
+        if (currentTileValue == 0 && nextTileValue != 0) {
+            setTileState(nextTileValue, row, col);
+            setTileState(0,row + offsetY, col + offsetX);
+            return true;
+        } else return false;
+    }
+
+    private void generateRandomNumber() {
+        if (Math.random() >= 0.9);
+    }
+    enum Direction {LEFT,RIGHT,UP,DOWN}
 }
