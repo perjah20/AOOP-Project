7b995f1ef53c76c2584e5d8a05ecbc1d4d86b3ab
Per Jahnstedt <perjah20@student.hh.se>
2022-05-22 23:03:01 +0200

    Removed constructor

--- src/main/java/sokoban/SokobanController.java
+++ src/main/java/sokoban/SokobanController.java
@@ -6,6 +6,4 @@ import tilegame.TileGameController;
  * The SokobanController class is a mediator between GameModel and GameGUI.
  */
 public class SokobanController extends TileGameController<SokobanGameModel> {
-    public SokobanController() {
-    }
 }
