58feb13a6355431913b0385783c66ccb26528a9d
Per Jahnstedt <perjah20@student.hh.se>
2022-05-27 21:05:30 +0200

    Removed test level and comment

--- src/main/java/game2048/Game2048.java
+++ src/main/java/game2048/Game2048.java
@@ -6,13 +6,6 @@ import tilegame.TileGame;
  * The Game2048 class holds the method for instantiating the game.
  */
 public class Game2048 extends TileGame<GameModel2048,GameGUI2048,GameController2048> {
-    private final int[][] start = {
-            {1024,1024,512,256},
-            {128,64,32,16},
-            {8,4,2,0},
-            {0,0,0,0},
-    };
-
     /**
      * Instantiates 2048 game session.
      */
@@ -20,7 +13,6 @@ public class Game2048 extends TileGame<GameModel2048,GameGUI2048,GameController2
         super(new GameModel2048(), new GameGUI2048(), new GameController2048());
         getTileGameModel().addGameObserver(new Console2048());
         getTileGameModel().updateObservers();
-        //getTileGameModel().updateGameGrid(start);
     }
 
     public static void main(String[] args) {
